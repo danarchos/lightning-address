99db3c1f444172c65d22b7d1cecefe6c479b1156
Daniel McGrane <danielmcgrane@Daniels-Mac-mini.local>
2022-02-03 20:47:15 +0000

    updated comments

--- auth/src/controllers/auth.js
+++ auth/src/controllers/auth.js
@@ -16,17 +16,21 @@ mongoose.connect(process.env.AUTH_DBHOST, {
 // SIGN UP - Creates user model, adds salt to password, creates a JWT
 exports.signup = asyncHandler(async (req, res) => {
   const newUser = new User(req.body);
+
   const salt = await bcrypt.genSalt(10);
   newUser.password = await bcrypt.hash(newUser.password, salt);
+
   const user = await newUser.save();
 
   const payload = { id: user.id, username: user.username };
   const token = jwt.sign(payload, JWT_SECRET, {
     expiresIn: JWT_EXPIRE,
   });
+
   res.status(200).json({ success: true, data: token });
 });
 
+// LOGIN
 exports.login = async (req, res) => {
   const { email, password } = req.body;
 
@@ -37,7 +41,6 @@ exports.login = async (req, res) => {
   }
 
   const passwordMatch = await bcrypt.compare(password, user.password);
-
   if (!passwordMatch) {
     res.status(401).json({ success: false, message: "Incorrect Password" });
   }
