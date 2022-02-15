85eb8e30ecffbf24d295421fb9fb3319cc075c3d
danny <daniel.mcgrane@zebedee.io>
2022-02-15 19:31:34 +0000

    added lightning typescript service

---
+++ lightning/src/index.ts
@@ -0,0 +1,7 @@
+import express from "express";
+
+const app = express();
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log(`LIGHTNING Service up and running`));
--- lightning/src/routes/lightning.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const { generateInvoice } = require("../controllers/lightning");
-
-const router = express.Router();
-
-router.route("/pay").post(generateInvoice);
-
-module.exports = router;
