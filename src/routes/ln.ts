b1c62c8f1fad66b80bb5be7404ea5ecdfc4c1533
Daniel McGrane <danielmcgrane@ip-192-168-1-43.ec2.internal>
2022-04-22 21:11:34 +0100

    changes to get wallet

--- src/routes/ln.ts
+++ src/routes/ln.ts
@@ -1,18 +1,13 @@
 import express from "express";
-import {
-  getWallet,
-  generateInvoice,
-  payInvoice,
-} from "../controllers/ln";
-import { events, receiveCallback } from '../controllers/events'
+import { getWallet, generateInvoice, payInvoice } from "../controllers/ln";
+import { events, receiveCallback } from "../controllers/events";
 
 const lnRouter = express.Router();
 
-lnRouter.route("/getWallet").get(getWallet);
+lnRouter.route("/get-wallet").get(getWallet);
 lnRouter.route("/invoice").get(generateInvoice);
 lnRouter.route("/pay-invoice").post(payInvoice);
 lnRouter.route("/wallet-recieve").post(receiveCallback);
 lnRouter.ws("/events", events);
 
-export default lnRouter
-
+export default lnRouter;
