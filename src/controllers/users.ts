6b839b3ea9be7040b4a72bcebaee52b6c88ca940
Daniel McGrane <danielmcgrane@ip-192-168-1-43.ec2.internal>
2022-04-24 00:25:37 +0100

    edited param

--- src/controllers/users.ts
+++ src/controllers/users.ts
@@ -1,48 +1,50 @@
-import { asyncHandler } from "../middlewares/asyncHandlerFn"
+import { asyncHandler } from "../middlewares/asyncHandlerFn";
 
-import { User } from "../models/User"
+import { User } from "../models/User";
 
 // GET user
 export const user = asyncHandler(async (req: any, res: any) => {
-    const { email } = req.query;
+  const { email } = req.query;
 
-    let user;
-    if (email) user = await User.findOne({ email });
+  let user;
+  if (email) user = await User.findOne({ email });
 
-    if (!user) {
-        res.status(400).json({ success: false, message: "No email found" });
-        return;
-    }
-    res.status(200).json({ success: true, user });
+  if (!user) {
+    res.status(400).json({ success: false, message: "No email found" });
+    return;
+  }
+  res.status(200).json({ success: true, user });
 });
 
 // Update username
 export const changeUsername = asyncHandler(async (req: any, res: any) => {
-    const { newUsername } = req.body
-    try {
-        await User.findOneAndUpdate({ id: req.decoded.id }, { username: newUsername })
-        res.status(200).json({ success: true, message: "Successfully updated username" });
-    } catch (err) {
-        res.status(500).json({ success: false, message: "Failed to change username" });
-    }
+  const { username } = req.body;
+  try {
+    await User.findOneAndUpdate({ id: req.decoded.id }, { username });
+    res
+      .status(200)
+      .json({ success: true, message: "Successfully updated username" });
+  } catch (err) {
+    res
+      .status(500)
+      .json({ success: false, message: "Failed to change username" });
+  }
 });
-
 
 // Update email
 export const changeEmail = asyncHandler(async (req: any, res: any) => {
-    const { newEmail } = req.body
-    if (!req.decoded.userId) {
-        res.status(404).json({ success: false, message: "Failed to change email" });
-        return
-    }
+  const { email } = req.body;
+  if (!req.decoded.userId) {
+    res.status(404).json({ success: false, message: "Failed to change email" });
+    return;
+  }
 
-    try {
-        await User.findOneAndUpdate({ id: req.decoded.userId }, { email: newEmail })
-        res.status(200).json({ success: true, message: "Successfully updated email" });
-    } catch (err) {
-        res.status(500).json({ success: false, message: "Failed to change email" });
-    }
+  try {
+    await User.findOneAndUpdate({ id: req.decoded.userId }, { email });
+    res
+      .status(200)
+      .json({ success: true, message: "Successfully updated email" });
+  } catch (err) {
+    res.status(500).json({ success: false, message: "Failed to change email" });
+  }
 });
-
-
-
