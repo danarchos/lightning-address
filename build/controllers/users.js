cd14a63fb4f805f0df8e47d1a3d2e7c2b545bf1c
Daniel McGrane <danielmcgrane@Daniels-MacBook-Pro.local>
2022-04-24 17:25:20 +0100

    did not build

--- build/controllers/users.js
+++ build/controllers/users.js
@@ -18,12 +18,14 @@ const User_1 = require("../models/User");
 const Pay_1 = __importDefault(require("../services/Pay"));
 // GET user
 exports.userInfo = (0, asyncHandlerFn_1.asyncHandler)((req, res) => __awaiter(void 0, void 0, void 0, function* () {
-    const { email } = req.query;
+    const { id } = req.query;
+    if (!id) {
+        res.status(404).json({ success: false, message: "No User id Provided" });
+    }
     let user;
-    if (email)
-        user = yield User_1.User.findOne({ email });
+    user = yield User_1.User.findOne({ id });
     if (!user) {
-        res.status(400).json({ success: false, message: "No email found" });
+        res.status(400).json({ success: false, message: "No user found" });
         return;
     }
     // Get Balance
